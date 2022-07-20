116fbaf7fffc5c4841761142c01ca3ed34ac4e7a
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-07-19 21:53:38 -0400

    destructuracion

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch','Dragon Ball']);
+    
+    const onAddCategory = () =>{
+        //setCategories([ ...categories,'Hola Mundo'])
+        setCategories( cat => [ ...categories,'Hola Mundo'] );
+    }
+
   return (
     <>
      <h1>GifExpertApp</h1>
+     <button onClick={ onAddCategory }>Agregar</button>
+     <ol>
+        { categories.map( category =>{
+            return <li key={ category }>{ category }</li>
+        }) }
+     </ol>
     </>
    
   )
