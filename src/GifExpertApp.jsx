cb748f39545320d862e37bd8a64f407ff04081b3
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-07-20 14:47:13 -0400

    comnicacion etre componentes

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,16 +5,11 @@ export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch','Dragon Ball']);
     
-    const onAddCategory = () =>{
-        //setCategories([ ...categories,'Hola Mundo'])
-        setCategories( cat => [ ...categories,'Hola Mundo'] );
-    }
-
+   
   return (
     <>
      <h1>GifExpertApp</h1>
-     <button onClick={ onAddCategory }>Agregar</button>
-     <AddCategory />   
+     <AddCategory  setCategories={ setCategories }/>   
      <ol>
         { categories.map( category =>{
             return <li key={ category }>{ category }</li>
--- src/addCategory.jsx
+++ src/addCategory.jsx
@@ -1,16 +1,19 @@
 import { useState  } from 'react';
 
-export const AddCategory = () =>{
+export const AddCategory = ({ setCategories }) =>{
 
     const [ inputValue, setInputValue ] = useState('One Punch');
 
-    const onInputChange = (event) =>{
-        console.log(event.target.value)
+    const onInputChange = ({ target }) =>{
+        setInputValue( target.value );
     }
 
     const onSubmit = ( event )  => {
         event.preventDefault();
-        console.log(inputValue)
+        if( inputValue.trim().length <= 1) return;
+        setCategories( categories => [ inputValue, ...categories ]);
+        setInputValue('');
+
     }
     return (
         <form onSubmit={ (event)  => onSubmit(event) }>
