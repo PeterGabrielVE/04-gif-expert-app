f7fddce8ab3fa9b1cb731073d248302c6c6c6c49
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-07-24 10:07:44 -0400

    New Component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AddCategory } from './addCategory';
+import { GifGrid } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
@@ -15,14 +16,10 @@ export const GifExpertApp = () => {
     <>
      <h1>GifExpertApp</h1>
      <AddCategory  
-     //setCategories={ setCategories }
       onNewCategory={ (value) => onAddCategory(value) }
      />   
-     <ol>
-        { categories.map( category =>{
-            return <li key={ category }>{ category }</li>
-        }) }
-     </ol>
+        { categories.map( ( category ) => ( <GifGrid key={ category } category={ category }/> )
+            ) }
     </>
    
   )
